3571eed8c5aeb30b9fadabed3fa8538e07b23f0e
Nick <nikmaunt@gmail.com>
2023-08-26 13:21:48 +0400

    feat: row1

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {GetKpisResponse} from "@/state/types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
@@ -7,7 +8,7 @@ export const api = createApi({
     reducerPath: "main",
     tagTypes:["Kpis"],
     endpoints: (build)=>  ({
-        getKpis: build.query<void, void>({
+        getKpis: build.query<Array<GetKpisResponse>, void>({
             query: ()=> "kpi/kpis/",
             providesTags: ["Kpis"]
         })
