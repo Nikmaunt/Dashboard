5e43de8effed4856198ead86a0f205337ebe3095
Nick <nikmaunt@gmail.com>
2023-09-01 10:56:19 +0400

    feat: product & transaction schema

--- server/index.js
+++ server/index.js
@@ -5,9 +5,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import KPI from "./models/KPI.js"
-import {kpis} from "./data/data.js"
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS*/
 dotenv.config()
@@ -24,6 +28,9 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+// app.use("/transaction", transactionRoutes);
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -39,7 +46,7 @@ mongoose
         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
         // await mongoose.connection.db.dropDatabase();
         // KPI.insertMany(kpis);
-        // Product.insertMany(products);
+        Product.insertMany(products);
         // Transaction.insertMany(transactions);
     })
     .catch((error) => console.log(`${error} did not connect`));
