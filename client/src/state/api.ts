778fe1d9996396fc1dbc813dbc03f24c861afe45
Nick <nikmaunt@gmail.com>
2023-09-15 17:47:45 +0400

    feat: transaction query

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,12 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {GetKpisResponse, GetProductsResponse} from "@/state/types";
+import {GetKpisResponse, GetProductsResponse, GetTransactionsResponse} from "@/state/types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: import.meta.env.VITE_BASE_URL
     }),
     reducerPath: "main",
-    tagTypes: ["Kpis", "Products"],
+    tagTypes: ["Kpis", "Products","Transactions"],
     endpoints: (build) => ({
         getKpis: build.query<Array<GetKpisResponse>, void>({
             query: () => "kpi/kpis/",
@@ -15,6 +15,10 @@ export const api = createApi({
         getProducts: build.query<Array<GetProductsResponse>, void>({
             query: () => "product/products/",
             providesTags: ["Products"]
+        }),
+        getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+            query: () => "transaction/transactions/",
+            providesTags: ["Transactions"]
         })
     })
 })
