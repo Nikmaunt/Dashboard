0d573e1f2dd92b77f6d53307ba072181d791f7d9
Nick <nikmaunt@gmail.com>
2023-09-16 01:32:04 +0400

    feat: row3

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,6 +1,16 @@
 import DashboardBox from "@/components/DashboardBox";
+import {useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery} from "@/state/api";
+import {useTheme} from "@mui/material";
+
 
 const Row3 = () => {
+    const { palette } = useTheme();
+    const pieColors = [palette.primary[800], palette.primary[500]];
+
+    const { data: kpiData } = useGetKpisQuery();
+    const { data: productData } = useGetProductsQuery();
+    const { data: transactionData } = useGetTransactionsQuery();
+
     return (
         <>
             <DashboardBox gridArea="g"></DashboardBox>
