396aa5d85518e1e0bbb50627688538c240726497
Nick <nikmaunt@gmail.com>
2023-10-09 19:54:33 +0400

    feat: spaces removed

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -3,7 +3,6 @@ import Row1 from "@/scenes/dashboard/Row1";
 import Row2 from "@/scenes/dashboard/Row2";
 import Row3 from "@/scenes/dashboard/Row3";
 
-
 const gridTemplateLargeScreens = `
   "a b c"
   "a b c"
